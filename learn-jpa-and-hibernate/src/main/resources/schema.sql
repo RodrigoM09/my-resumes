402789af63c6af5b2e82eb11c808ce4a62f51401
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-18 11:26:29 -0800

    packages fix

--- learn-jpa-and-hibernate/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists course(
-                       id bigint not null,
-                       name varchar(255) not null,
-                       author varchar(255) not null,
-                       primary key (id)
-);
